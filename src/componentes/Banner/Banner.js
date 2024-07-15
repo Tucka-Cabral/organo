0e462d467d4265c1397e964d78fff5c7457ef20d
Thais Cabral <tucka.cabral@gmail.com>
2024-07-14 22:14:55 -0300

    Primeiro Commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner do Organa" className='banner__img'/>
+        </header>
+    )
+
+}
+
+export default Banner
